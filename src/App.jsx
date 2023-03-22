95f78485dfd68f555be86a54895af21023cbeefe
Shihab Ahmed Pranto <sa.pranto16@gmail.com>
2023-03-22 09:06:14 +0600

    Created Routing and all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import './index.css'
 import './App.css'
+import { Route, Routes } from "react-router-dom";
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard';
+import Users from './pages/Users';
 export default function App() {
   return (
-    <h2 className="text-3xl font-bold underline">
-      Hello world!
-    </h2>
+    <>
+    <Routes>
+      <Route path={'/signup'} element={<SignUp/>} />
+      <Route path={'/signin'} element={<SignIn/>} />
+      <Route path={'/'} element={<Dashboard/>} />
+      <Route path={'/users'} element={<Users/>} />
+    </Routes>
+    </>
   )
 }
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <>
+    <div>SignUp</div>
+    <label htmlFor="">Name</label>
+    <input type="text" placeholder='Name' />
+    <label htmlFor="">Email</label>
+    <input type="text" placeholder='Email' />
+    <label htmlFor="">Password</label>
+    <input type="text" placeholder='Password' />
+    </>
+  )
+}
+
+export default SignUp
