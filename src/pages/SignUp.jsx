5b1a00deae62dec6185dd368f2e2793b8c713fa1
Shihab Ahmed Pranto <sa.pranto16@gmail.com>
2023-03-22 21:33:53 +0600

    Sign Up Page Desgin Created

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
 import { FaApple } from "react-icons/fa";
+import { RiUserSmileLine } from "react-icons/ri";
 import SingleLine from "./components/SingleLine";
+import PasswordBar from "./components/PasswordBar";
 const SignUp = () => {
   return (
     <div class="flex items-center justify-center my-32 text-center">
@@ -20,15 +22,34 @@ const SignUp = () => {
             <span>Sign Up with Apple ID</span>
           </div>
         </div>
-          <SingleLine/>
+        <SingleLine />
         <div className="">
-          <input type="text" placeholder="Your Email" />
+          <input type="text" placeholder="Your Email" className="field" />
+        </div>
+        <div className="">
+          <input type="text" placeholder="Your Name" className="field" />
         </div>
         <div>
-          <input type="text" placeholder="Your Name" />
+          <input type="text" placeholder="Create Password" className="field" />
         </div>
-        <div>
-          <input type="text" placeholder="Create Password" />
+        <PasswordBar />
+        <div className="flex items-center mb-4">
+          <input
+            id="default-checkbox"
+            type="checkbox"
+            defaultValue=""
+            className="w-4 h-4 text-primary bg-primary border-primary rounded focus:ring-blue-500"
+          />
+          <label
+            htmlFor="default-checkbox"
+            className="ml-2 text-xs text-primary"
+          >
+            I agree to the Terms & Conditions
+          </label>
+        </div>
+        <button className="bg-secondary text-white text-xs w-[22rem] h-[2.5rem] rounded-lg">Sign Up</button>
+        <div className="text-xs mt-4 text-primary">
+          Already Have an account? <span className="text-blue-600 font-semibold">Sign In</span>
         </div>
       </div>
     </div>
---
+++ src/pages/components/PasswordBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const PasswordBar = () => {
+  return (
+    <div className="flex gap-3 -mt-5 justify-center">
+      <div className="bar"></div>
+      <div className="bar"></div>
+      <div className="bar"></div>
+      <div className="bar"></div>
+      <div className="bar"></div>
+      <div className="w-[3rem] rounded-xl h-[3px] my-8 bg-primary-box border-0"></div>
+    </div>
+  );
+};
+
+export default PasswordBar;
