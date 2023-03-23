bbcc6b46ff48d9207e9f9f0f2e6e9baf243fe5bd
Shihab Ahmed Pranto <sa.pranto16@gmail.com>
2023-03-24 00:16:00 +0600

    Full 4 Pages Desgin Done

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,36 +1,51 @@
-import React from 'react'
-import { RxDashboard } from "react-icons/rx";
-import { HiOutlineUser } from "react-icons/hi";
-import { TbReportAnalytics } from "react-icons/tb";
+import React, { useState } from 'react';
+import { RxDashboard } from 'react-icons/rx';
+import { HiOutlineUser } from 'react-icons/hi';
+import { TbReportAnalytics } from 'react-icons/tb';
+import { Link } from 'react-router-dom';
+
 const SideBar = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleOnClick = (index) => {
+    setActiveIndex(index === activeIndex ? null : index);
+  };
+
+  const renderIcon = (Icon) => (
+    <span>
+      <Icon className="text-lg text-primary-text mt-[3px]" />
+    </span>
+  );
+
+  const renderMenuItem = (index, Icon, title) => (
+    <div
+      className={`dash ${
+        activeIndex === index ? 'bg-gray-200' : ''
+      }`}
+      onClick={() => handleOnClick(index)}
+    >
+      {renderIcon(Icon)}
+      <h5 className="text-primary-text">{title}</h5>
+    </div>
+  );
+
   return (
     <>
-    <div className='container pt-4 border-r w-44 h-screen px-4 mx-2'>
-      <img src="./logo.png" alt="" width={120} className='mx-2'/>
-      <div className='mt-2 mb-4 mx-2'>
-      <span className='text-primary-text text-xs'>PAGES</span>
+      <div className="container pt-4 border-r w-56 h-screen px-4 mx-2">
+        <img src="./logo.png" alt="" width={120} className="mx-2" />
+        <div className="mt-2 mb-4 mx-2">
+          <span className="text-primary-text text-xs">PAGES</span>
+        </div>
+        <Link to='/'>
+        {renderMenuItem(0, RxDashboard, 'Dashboard')}
+        </Link>
+        <Link to='/users'>
+        {renderMenuItem(1, HiOutlineUser, 'Users')}
+        </Link>
+        {renderMenuItem(2, TbReportAnalytics, 'Sales')}
       </div>
-      <div className='dash '>
-        <span>
-        <RxDashboard className='text-lg text-primary-text mt-[3px]'/>
-        </span>
-        <h5 className='text-primary-text'>Dashboard</h5>
-      </div>
-      <div className='dash'>
-        <span>
-        <HiOutlineUser className='text-lg text-primary-text mt-[3px]'/>
-        </span>
-        <h5 className='text-primary-text'>Users</h5>
-      </div>
-      <div className='dash'>
-        <span>
-        <TbReportAnalytics className='text-lg text-primary-text mt-[3px]'/>
-        </span>
-        <h5 className='text-primary-text'>Sales</h5>
-      </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default SideBar
+export default SideBar;
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { RxDashboard } from 'react-icons/rx';
+import { HiOutlineUser } from 'react-icons/hi';
+import { TbReportAnalytics } from 'react-icons/tb';
+import { Link } from 'react-router-dom';
+import Search from '../components/Search'
 
 const Users = () => {
+  const [activeIndex, setActiveIndex] = useState(1);
+
+  const handleOnClick = (index) => {
+    setActiveIndex(index === activeIndex ? null : index);
+  };
+
+  const renderIcon = (Icon) => (
+    <span>
+      <Icon className="text-lg text-primary-text mt-[3px]" />
+    </span>
+  );
+
+  const renderMenuItem = (index, Icon, title) => (
+    <div
+      className={`dash ${
+        activeIndex === index ? 'bg-gray-200' : ''
+      }`}
+      onClick={() => handleOnClick(index)}
+    >
+      {renderIcon(Icon)}
+      <h5 className="text-primary-text">{title}</h5>
+    </div>
+  );
+
   return (
-    <div>Users</div>
+    <div className='flex'>
+      <div className="container pt-4 border-r w-56 h-screen px-4 mx-2">
+        <img src="./logo.png" alt="" width={120} className="mx-2" />
+        <div className="mt-2 mb-4 mx-2">
+          <span className="text-primary-text text-xs">PAGES</span>
+        </div>
+        <Link to='/'>
+        {renderMenuItem(0, RxDashboard, 'Dashboard')}
+        </Link>
+        <Link to='/users'>
+        {renderMenuItem(1, HiOutlineUser, 'Users')}
+        </Link>
+        {renderMenuItem(2, TbReportAnalytics, 'Sales')}
+      </div>
+      <div>
+      <Search/>
+      </div>
+    </div>
   )
 }
 
