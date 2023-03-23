7624224d0820d24ef40d2b8581618d787c7c2e89
Shihab Ahmed Pranto <sa.pranto16@gmail.com>
2023-03-23 20:55:53 +0600

    Made Dashboard and Users Private Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
-import './index.css'
-import './App.css'
+import "./index.css";
+import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import Dashboard from './pages/Dashboard';
-import Users from './pages/Users';
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Users from "./pages/Users";
+import PrivateOutlet from "./pages/components/PrivateOutlet";
+
 export default function App() {
   return (
     <>
-    <Routes>
-      <Route path={'/signup'} element={<SignUp/>} />
-      <Route path={'/signin'} element={<SignIn/>} />
-      <Route path={'/'} element={<Dashboard/>} />
-      <Route path={'/users'} element={<Users/>} />
-    </Routes>
+      <Routes>
+        <Route path={"/signup"} element={<SignUp />} />
+        <Route path={"/signin"} element={<SignIn />} />
+        <Route path="/*" element={<PrivateOutlet />}>
+          <Route path="dashboard" element={<Dashboard/>} />
+          <Route path="users" element={<Users/>} />
+        </Route>
+      </Routes>
     </>
-  )
+  );
 }
